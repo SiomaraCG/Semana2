f576c6bf9b2efeea9b78ee28856f1f272fb327c0
SiomaraCG <desireesiomara@gmail.com>
2024-05-08 19:44:57 -0500

    Ejercicio 1 en clase

---
+++ arreglo.java
@@ -0,0 +1,15 @@
+public class Main {
+    // arreglo undimensional
+    private int [] arreglo1 = new int [10];
+
+    private int [] arreglo2 = {1,2,3,4};
+
+    // Uun arreglo es una colección secuencial de elementos que tienen el mismo tipo
+    Object[] arreglo3 = {"22", 22, Boolean.FALSE, Long.parseLong("22")};
+
+    // multidimensional
+    private int [] [] [] arreglo4 = new 
+
+    
+    
+}
